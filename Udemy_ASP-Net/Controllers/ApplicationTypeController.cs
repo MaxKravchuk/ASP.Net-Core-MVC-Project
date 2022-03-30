b8dbae32d8216971464bbb949322a987944399d0
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-03-30 16:31:46 +0300

    Index and Create functionality

--- Udemy_ASP-Net/Controllers/ApplicationTypeController.cs
+++ Udemy_ASP-Net/Controllers/ApplicationTypeController.cs
@@ -3,14 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Udemy_ASP_Net.Data;
+using Udemy_ASP_Net.Models;
 
 namespace Udemy_ASP_Net.Controllers
 {
     public class ApplicationTypeController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public ApplicationTypeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<ApplicationType> objList = _db.ApplicationType;
+            return View(objList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        {
+            _db.ApplicationType.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
