d132342c4319540577b39d4f6c58bca34a59cc15
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-03-30 17:44:00 +0300

    Edit / Delete Functionality

--- Udemy_ASP-Net/Controllers/CategoryController.cs
+++ Udemy_ASP-Net/Controllers/CategoryController.cs
@@ -39,5 +39,52 @@ namespace Udemy_ASP_Net.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var obj = _db.Category.Find(id);
+
+            if (obj == null) return NotFound();
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (!ModelState.IsValid) return View(obj);
+            _db.Category.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var obj = _db.Category.Find(id);
+
+            if (obj == null) return NotFound();
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id==null) return NotFound();
+            var obj = _db.Category.Find(id);
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
