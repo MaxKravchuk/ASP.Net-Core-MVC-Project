277feff63f91dfcdeb993d090c6c702ede3d25b8
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-04-05 18:43:18 +0300

    New Main page

--- Udemy_ASP-Net/Controllers/HomeController.cs
+++ Udemy_ASP-Net/Controllers/HomeController.cs
@@ -1,26 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Udemy_ASP_Net.Data;
 using Udemy_ASP_Net.Models;
+using Udemy_ASP_Net.Models.ViewModels;
 
 namespace Udemy_ASP_Net.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType),
+                Categories = _db.Category
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ Udemy_ASP-Net/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Udemy_ASP_Net.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
