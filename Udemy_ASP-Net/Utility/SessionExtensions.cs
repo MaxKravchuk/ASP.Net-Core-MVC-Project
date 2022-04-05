a3ce0459a16b17e60bd4f0078620d85df1eb6545
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-04-05 20:23:46 +0300

    Session logic .1

---
+++ Udemy_ASP-Net/Utility/SessionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Udemy_ASP_Net.Utility
+{
+    public class SessionExtensions
+    {
+    }
+}
